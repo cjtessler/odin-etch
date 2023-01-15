71f804d3deeaed80f881345a93f3647bfd3a71af
Cody Tessler <codytessler@gmail.com>
2023-01-15 10:22:31 -0500

    hover to change color implemented

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ const canvas = document.querySelector('.canvas');
 let numCells = 16*16;
 
 for (let i = 0; i < numCells; i++) {
+    // create cell
     let cell = document.createElement("div");
     cell.classList.add("cell");
+
+    // attach event listener
+    cell.addEventListener('mouseenter', colorCell);
+
+    // Add cell to dom
     canvas.append(cell);
 }
 
+function colorCell(e) {
+    this.classList.add('hovered');
+}
