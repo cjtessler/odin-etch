e27fe0883d5dab5499fa9d7674aa5ce127a3f8ab
Cody Tessler <codytessler@gmail.com>
2023-01-25 22:39:11 -0500

    Implemented grid-sizing

--- script.js
+++ script.js
@@ -1,22 +1,52 @@
-
+// Grid Size Slider
+const sizeInput = document.getElementById('sizeInput');
+const slider = document.getElementById('slider')
 const canvas = document.querySelector('.canvas');
-let numCells = 16*16;
+const root = document.querySelector(':root');
 
-for (let i = 0; i < numCells; i++) {
-    // create cell
-    let cell = document.createElement("div");
-    cell.classList.add("cell");
+// Setup
+root.style.setProperty('--grid-size', 16);
+drawGrid(16);
 
-    // attach event listener
-    cell.addEventListener('mouseenter', colorCell);
+slider.oninput = () => {
+    const value = slider.value;
+    sizeInput.value = value;
+    root.style.setProperty('--grid-size', value);
+    drawGrid(sizeInput.value);
+};
 
-    // Add cell to dom
-    canvas.append(cell);
+sizeInput.onchange = () => {
+    const value = sizeInput.value;
+    slider.value = value;
+    root.style.setProperty('--grid-size', value);
+    drawGrid(value);
 }
+
+
+function drawGrid(width) {
+
+    const cells = [];
+
+    let numCells = width ** 2;
+    for (let i = 0; i < numCells; i++) {
+        // create cell
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+
+        // attach event listener
+        cell.addEventListener('mouseenter', colorCell);
+
+        // Add cell to dom
+        cells.push(cell);
+        // canvas.append(cell);
+    }
+
+    canvas.replaceChildren(...cells);
+}
+
 // Clear Button
 const clearButton = document.getElementById('clearButton');
 clearButton.addEventListener('click', () => {
-    console.log(canvas.children);
     const children = canvas.children;
     for (const child of children) {
         child.classList.remove('hovered');
